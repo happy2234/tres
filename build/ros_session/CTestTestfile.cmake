7961e118dfeec928bfc0f9acb3201bafbdceae90
root <root@ert.myguest.virtualbox.org>
2024-11-09 18:48:53 +0530

    Initial commit

---
+++ build/ros_session/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/ros_ws/src/ros_session
+# Build directory: /root/ros_ws/build/ros_session
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
